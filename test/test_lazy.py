6f335b8f5654b57b55a095d030c3daf7a1468870
3noch <elliot@3noch.com>
2013-02-25 14:37:36 -0500

    Install local package before testing

--- test/test_lazy.py
+++ test/test_lazy.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
 from stringlike.lazy import LazyString, CachedLazyString
 from unittest import main, TestCase
 
