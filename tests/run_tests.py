591aaa938c22b797fc6bbeb5050ec489cc966a47
3noch <elliot@3noch.com>
2014-07-09 11:35:05 -0400

    Make running unit tests more friendly

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Hack to allow us to run tests before installing.
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..')))
+
 from unittest import main
 
 from test_core import *
